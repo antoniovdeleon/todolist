224f7b7331482882773af63fd7a1cf99a1bef81d
Antonio de Leon <tonichi@gmail.com>
2015-06-30 11:10:11 -0700

    homework submission

---
+++ read.md
@@ -0,0 +1,3 @@
+add tasks to the list
+see that the tasks are currently on the list
+check off tasks that are completed
---
+++ script.js
@@ -0,0 +1,55 @@
+$(function() {
+
+ var $shoppingUL = $('#shopping_list');
+ console.log(typeof $shoppingUL);
+
+
+ var $newItemForm = $('#new_shopping_item');
+
+ var $itemNameBlank = $('#item_name');
+
+ $newItemForm.on('submit', function(event) {
+   event.preventDefault();
+   console.log($('#item_name').val());
+   var listItem = $('#item_name').val();
+   console.log(listItem)
+
+   // store our new shopping list item
+   //if (listItem)
+ //  if listItem
+   if (listItem===""){
+      console.log("this has nothing inside");
+      alert("Enter something!");
+      } else {
+      console.log("this has value");
+      }
+   shoppingList.push(listItem);
+
+   // clear the form
+   $itemNameBlank.val("");
+
+   //append our new item to the page's shopping list
+   $shoppingUL.append('<li class="item">' + listItem + '</li>');
+ });
+
+ $shoppingUL.on('click', '.item', function() {
+   $(this).addClass('purchased');
+   $(this).animate({opacity: '0.60'}, 1000);
+   $(this).wrap('<strike>');
+   console.log(typeof $shoppingUL)
+ });
+
+ $shoppingUL.on('dblclick', '.item', function() {
+   $(this).addClass('purchased');
+   $(this).animate({opacity: '0.10'}, 1000);
+   $(this).wrap('<strike>');
+   console.log(typeof $shoppingUL)
+   
+   $( "#foo").unbind( "click" );
+ });
+
+
+
+});
+
+var shoppingList = [];
